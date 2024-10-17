1b1104d22eccc1da77edb5c3151c238a2fa7c532
Murat <m_kairbek@outlook.com>
2024-10-17 23:38:49 +0500

    restructured the struct, commented some fields;

--- bmp/001Bmp.go
+++ bmp/001Bmp.go
@@ -5,12 +5,11 @@ package bmp
 type bmp struct {
 	fileHeader *fileHeader
 	dibHeader  *dibHeader
-	colorTable colorTable
 	pixelArray [][]byte
 }
 
 // Device independent bitmap header
-// this header correspond to BITMAPV5HEADER
+// header corresponds to BITMAPINFOHEADER version for 24 bits
 // see: https://upload.wikimedia.org/wikipedia/commons/7/75/BMPfileFormat.svg
 type dibHeader struct {
 	size                  uint32
@@ -24,19 +23,38 @@ type dibHeader struct {
 	verticalResolution    uint32
 	colorsNumber          uint32
 	importantColorsNumber uint32
-	redChannel            uint32
-	greenChannel          uint32
-	blueChannel           uint32
-	alphaChannel          uint32
-	colorSpaceType        uint32
-	colorSpaceEndpoints   uint32
-	gammaRedChannel       uint32
-	gammaGreenChannel     uint32
-	gammaBlueChannel      uint32
-	intent                uint32
-	iccProfileData        uint32
-	iccProfileSize        uint32
-	reserved              uint32
 }
 
-type colorTable []byte
+// Fields will be added in future to correspond BITMAPV5HEADER
+// redChannel            uint32
+// greenChannel          uint32
+// blueChannel           uint32
+// alphaChannel          uint32
+// colorSpaceType        uint32
+// colorSpaceEndpoints   uint32
+// gammaRedChannel       uint32
+// gammaGreenChannel     uint32
+// gammaBlueChannel      uint32
+// intent                uint32
+// iccProfileData        uint32
+// iccProfileSize        uint32
+// reserved              uint32
+
+// Compression methods
+// see (https://en.wikipedia.org/wiki/BMP_file_format#DIB_header_(bitmap_information_header))
+// Will be added in future
+// const (
+// 	BI_RGB = iota
+// 	BI_RLE8
+// 	BI_RLE4
+// 	BI_BITFIELDS
+// 	BI_JPEG
+// 	BI_PNG
+// 	BI_ALPHABITFIELDS
+// 	BI_CMYK     = 11
+// 	BI_CMYKRLE8 = 12
+// 	BI_CMYKRLE4 = 13
+// )
+
+// Color table for color pallete <= 8 bits (will be added in future)
+// type colorTable []byte
